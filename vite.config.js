6e19844ac49e63eb490160976fed8fdefe7e4fe7
nicholaspsmith <npsmith1990@gmail.com>
2025-12-25 19:41:31 -0500

    Configure Vite with strict port binding

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,8 @@ import { sveltekit } from '@sveltejs/kit/vite';
 const config = {
 	plugins: [sveltekit()],
 	server: {
+		port: 5173,
+		strictPort: true,
 		fs: {
 			allow: ['./static/', './renderer-src/'],
 		},
